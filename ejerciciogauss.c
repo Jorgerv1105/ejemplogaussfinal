22f22953b534118a29ce397ba95312b1245f7c97
unknown <jorge.ramos.vasquez@udla.edu.ec>
2023-10-12 23:51:47 -0500

    Procedimiento

--- ejerciciogauss.c
+++ ejerciciogauss.c
@@ -5,5 +5,15 @@ int main()
     printf("\nPorgrama para calcular la sumatoria de numeros naturales con el metodo de Gauss"); 
     printf("\nIngrese un numero natural: "); 
     scanf(" %d", &n);
+    if (n==1){
+
+    printf("\nLa sumatoria del numero 1 es 1 \n");
+
+    }
+     else{
+        for (i=1; i<=n; i++)
+        {
+            suma=(n*(n+1))/2;
+        }
 return 0;
 }
