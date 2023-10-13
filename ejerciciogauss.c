8fc1c6a9f59745aa7dcdb32d3115f2fa5f616ddd
unknown <jorge.ramos.vasquez@udla.edu.ec>
2023-10-12 23:50:17 -0500

    Ingreso de datos

--- ejerciciogauss.c
+++ ejerciciogauss.c
@@ -2,6 +2,8 @@
 int main() 
 { 
     int n, i, suma ; 
-
+    printf("\nPorgrama para calcular la sumatoria de numeros naturales con el metodo de Gauss"); 
+    printf("\nIngrese un numero natural: "); 
+    scanf(" %d", &n);
 return 0;
 }
